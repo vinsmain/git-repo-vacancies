8ce4f05541c2c72ccc508acd89bd27e83692317e
VINs <vinsmain@gmail.com>
2017-05-18 21:28:40 +0500

    Delete trash libs I will be use JSON!

--- src/main/java/ru/vacancies/StartClass.java
+++ src/main/java/ru/vacancies/StartClass.java
@@ -1,13 +1,6 @@
 package ru.vacancies;
 
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import ru.vacancies.parser.Parser;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class StartClass {
 
@@ -15,54 +8,7 @@ public class StartClass {
     public static void main(String[] args) {
 
         Parser parser = new Parser();
-        final CopyOnWriteArrayList<String> urlArray = new CopyOnWriteArrayList<String>();
-
-        try {
-            Document page = parser.getPage("https://ekb.zarplata.ru/vacancy?offset=350");
-            Elements vacancyURL = page.select("h2[class=ui header tiny]");
-            System.out.println(vacancyURL);
-
-            for (final Element aURL : vacancyURL) {
-                new Thread(new Runnable() {
-                    public void run() {
-                        Document vacPage = null;
-                        try {
-                            vacPage = new Parser().getPage1("https://ekb.zarplata.ru" + aURL.select("a").attr("href"));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        String name = null;
-                        Element element;
-                        try {
-
-                            if (vacPage != null) {
-                                Elements elements = vacPage.select("div[class=contacts_HtZXz ui list]");
-                                //System.out.println("12345 " + elements);
-                                element = elements.select("div[class=item]").first();
-                                //System.out.println(element);
-                                name = element.text();
-                                System.out.println(name);
-                            }
-                        } catch (NullPointerException e) {
-                            System.out.println("12345 " + "https://ekb.zarplata.ru" + aURL.select("a").attr("href"));
-                            e.printStackTrace();
-                        }
-
-
-                        //urlArray.add(name);
-                    }
-                }).start();
-                break;
-
-                //System.out.println("https://ekb.zarplata.ru" + aURL.select("a").attr("href"));
-            }
-            /*
-            for (String url : urlArray) {
-                System.out.println(url);
-            }*/
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        String json = parser.getJSON("https://api.zp.ru/v1/vacancies?offset=0&geo_id=994&limit=50");
+        System.out.println(json);
     }
 }
--- src/main/java/ru/vacancies/parser/Parser.java
+++ src/main/java/ru/vacancies/parser/Parser.java
@@ -1,51 +1,21 @@
 package ru.vacancies.parser;
 
-import com.google.common.collect.FluentIterable;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
+import java.net.URL;
 
 public class Parser {
+    /*
+    //https://api.zp.ru/v1/vacancies?offset=0&geo_id=994&limit=50 API НЕ УДАЛЯТЬ!!!
+    */
 
-    public Document getPage(String url) throws IOException {
-        System.setProperty("phantomjs.binary.path", "src/main/resources/phantomjs/bin/phantomjs.exe");
-        WebDriver ghostDriver = new PhantomJSDriver();
-        ghostDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    public String getJSON(String url) {
         try {
-            ghostDriver.get(url);
-            //WebElement dynamicElement = (new WebDriverWait(ghostDriver, 10))
-             //       .until(ExpectedConditions.presenceOfElementLocated(By.className("contacts_HtZXz ui list")));
-
-            return Jsoup.parse(ghostDriver.getPageSource());
-        } finally {
-            ghostDriver.quit();
+            return Jsoup.parse(new URL(url), 3000).text();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return null;
     }
-
-    public Document getPage1(String url) throws IOException {
-        System.setProperty("phantomjs.binary.path", "src/main/resources/phantomjs/bin/phantomjs.exe");
-        WebDriver ghostDriver = new PhantomJSDriver();
-        ghostDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        try {
-            ghostDriver.get(url);
-            WebElement dynamicElement = (new WebDriverWait(ghostDriver, 3))
-                    .until(ExpectedConditions.presenceOfElementLocated(By.className("ui container")));
-
-            return Jsoup.parse(ghostDriver.getPageSource());
-        } finally {
-            ghostDriver.quit();
-        }
-    }
-
-
 }
